0f1367676e85b09f5e8a523d7a97c8139f3abf4b
[SamirRehm] <srehmt@gmail.com>
2017-07-30 00:17:53 -0400

    Attempting non linear regression

--- Controller.R
+++ Controller.R
@@ -29,11 +29,28 @@ Controller$methods(
 controller = Controller$new()
 trueFunc = controller$generateRandomFunction()
 data = controller$generateCoordinates(1000, trueFunc)
+
 xList = data[[1]]
 yList = data[[2]]
+
 plot(unlist(xList), unlist(yList))
+
 calculator = StatCalc$new()
-approxFunc = controller$generateRandomFunction()
-calculator$meanSquaredError(xList, yList, approxFunc)
+approxFuncSlope = list(10)
+approxFuncSlopeSquared = list(10)
+approxFuncInt = list(10)
+ErrorList = list(10)
+for(j in 1:10)  {
 
+  approxFunc = controller$generateRandomFunction()
+  approxFuncSlope[[j]] = approxFunc$slope
+  approxFuncInt[[j]] = approxFunc$yInt
+  approxFuncSlopeSquared[[j]] = (approxFunc$slope)^2
 
+  ErrorList[[j]] = calculator$meanSquaredError(xList, yList, approxFunc)
+}
+ErrorList2=unlist(ErrorList)
+Slope2 = unlist(approxFuncSlope)
+
+model <- nls(ErrorList2 ~ b1*(Slope2)^2+b2*Slope2 + b3,start = list(b1 = 1,b2 = 3, b3 = 0))
+print(model)
